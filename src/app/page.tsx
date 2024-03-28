ae4002c1285765b0464b5417f1346d116f1c4316
heion31 <mainit.noah3113@gmail.com>
2024-03-29 00:57:24 +0800

    clerk components setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignOutButton,
+} from "@clerk/clerk-react";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Hello World</Button>
+      <SignedIn>
+        <SignOutButton>
+          <Button>Sign Out</Button>
+        </SignOutButton>
+      </SignedIn>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button>Sign In</Button>
+        </SignInButton>
+      </SignedOut>
+
+      {/* <SignInButton mode="modal" />
+      <Button>Hello World</Button> */}
     </main>
   );
 }
