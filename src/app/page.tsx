ff757e7c984e806c6a3256ae56472d58b3d20d6b
heion31 <mainit.noah3113@gmail.com>
2024-03-30 13:57:56 +0800

    add loading indicator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { api } from "../../convex/_generated/api";
 import { UploadButton } from "./upload-button";
 import { FileCard } from "./file-card";
 import Image from "next/image";
+import { Loader2 } from "lucide-react";
 
 export default function Home() {
   // Hooks for fetching organization and user data
@@ -20,10 +21,21 @@ export default function Home() {
 
   // hooks for fetching files
   const files = useQuery(api.files.getFiles, orgId ? { orgId } : "skip");
+  // Determine if files are still loading
+  const isLoading = files === undefined;
 
   return (
     <main className="container mx-auto pt-12">
-      {files && files.length === 0 && (
+      {/* Display loading indicator while files are loading */}
+      {isLoading && (
+        <div className="flex flex-col gap-8 w-full items-center mt-24">
+          <Loader2 className="h-32 w-32 animate-spin text-gray-500" />
+          <div className="text-2xl">Loading your images...</div>
+        </div>
+      )}
+
+      {/* Display message and upload button when no files are available */}
+      {!isLoading && files.length === 0 && (
         <div className="flex flex-col gap-8 w-full items-center mt-24">
           <Image
             alt="A girl with a folder icon"
@@ -31,21 +43,21 @@ export default function Home() {
             height="300"
             src="/empty.svg"
           />
-          <div className="text-2xl">You have no files, upload one now</div>
+          <div className="text-2xl">No files found. Upload one now!</div>
           <UploadButton />
         </div>
       )}
 
-      {files && files.length > 0 && (
+      {/* Display files if not loading and files exist */}
+      {!isLoading && files.length > 0 && (
         <>
           <div className="flex justify-between items-center mb-8">
             <h1 className="text-4xl font-bold">Your Files</h1>
-
             <UploadButton />
           </div>
 
           <div className="grid grid-cols-4 gap-4">
-            {/* Display uploaded files */}
+            {/* Render uploaded files */}
             {files?.map((file) => {
               return <FileCard key={file._id} file={file} />;
             })}
