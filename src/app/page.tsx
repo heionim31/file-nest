86d0d1c0979b7a53bc3e7447f364bca8ae33f300
heion31 <mainit.noah3113@gmail.com>
2024-03-30 13:44:53 +0800

    add iniital image placeholder when there are no uploaded files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import { UploadButton } from "./upload-button";
 import { FileCard } from "./file-card";
+import Image from "next/image";
 
 export default function Home() {
   // Hooks for fetching organization and user data
@@ -22,18 +23,35 @@ export default function Home() {
 
   return (
     <main className="container mx-auto pt-12">
-      <div className="flex justify-between items-center mb-8">
-        <h1 className="text-4xl font-bold">Your Files</h1>
+      {files && files.length === 0 && (
+        <div className="flex flex-col gap-8 w-full items-center mt-24">
+          <Image
+            alt="A girl with a folder icon"
+            width="300"
+            height="300"
+            src="/empty.svg"
+          />
+          <div className="text-2xl">You have no files, upload one now</div>
+          <UploadButton />
+        </div>
+      )}
 
-        <UploadButton />
-      </div>
+      {files && files.length > 0 && (
+        <>
+          <div className="flex justify-between items-center mb-8">
+            <h1 className="text-4xl font-bold">Your Files</h1>
 
-      <div className="grid grid-cols-4 gap-4">
-        {/* Display uploaded files */}
-        {files?.map((file) => {
-          return <FileCard key={file._id} file={file} />;
-        })}
-      </div>
+            <UploadButton />
+          </div>
+
+          <div className="grid grid-cols-4 gap-4">
+            {/* Display uploaded files */}
+            {files?.map((file) => {
+              return <FileCard key={file._id} file={file} />;
+            })}
+          </div>
+        </>
+      )}
     </main>
   );
 }
